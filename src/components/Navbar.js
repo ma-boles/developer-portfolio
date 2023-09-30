2c16a29e289263ac82c1dcb38fc51a96077b9d7f
ma-boles <ma_boles@hotmail.com>
2023-09-30 11:07:12 -0700

    git commit m- "smooth scroll added"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
+/*import { Link } from "react-router-dom";*/
+import { useEffect, useRef } from "react";
 import "../styles/Navbar.css";
 import logo from "../images/Grey-Yourlo-Logo.jpg";
 import github from "../images/GitHub_Logo.png";
@@ -9,6 +10,42 @@ import IGLogo from "../images/Instagram_logo.svg.png"
 
 
 export default function Navbar () {
+    
+    const handleClick = (anchor) => () => {
+      const id = `${anchor}-section`;
+      const element = document.getElementById(id);
+      if (element) {
+        element.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    };
+  
+    const headerRef = useRef(null);
+  
+    useEffect(() => {
+      let prevScrollPos = window.scrollY;
+  
+      const handleScroll = () => {
+        const currScrollPos = window.scrollY;
+        const currHeaderElement = headerRef.current;
+  
+        if (!currHeaderElement)
+          return;
+        
+        if (prevScrollPos > currScrollPos)
+          currHeaderElement.style.transform = "translateY(0)";
+        else
+          currHeaderElement.style.transform = "translateY(-200px)";
+        
+        prevScrollPos = currScrollPos;
+      };
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
 
     window.onscroll = function() {scrollFunction()};
 
@@ -20,6 +57,7 @@ export default function Navbar () {
         }
     }
 
+    /*<a href ="/#contact-me" onClick={handleClick("contactme")}>Contact Me</a>*/
 
 
     return (
@@ -28,12 +66,10 @@ export default function Navbar () {
             <img className="logo"src={logo}></img>
         <div className="navbar">
             <ul>
-                <li><a href="index.html">
-                    Home</a>
-                    </li>
-                <li><a href="index.html">About</a></li>
-                <li><a href="index.html">Projects</a></li>
-                <li><a href="index.html">Contact</a></li>
+                <li><a href="/#home" onClick={handleClick("home")}>Home</a></li>
+                <li><a href="/#about" onClick={handleClick("about")}>About</a></li>
+                <li><a href="/#projects" onClick={handleClick("projects")}>Projects</a></li>
+                <li><a href="/#contact" onClick={handleClick("contact")}>Contact</a></li>
                 <li className="dropdown">
                     <button className="dropdown--menu">Socials</button>
                         <div className="dropdown-content">
