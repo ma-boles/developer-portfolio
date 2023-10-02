19bad7821c679a36384c9dfe625be28c3098a4f8
ma-boles <ma_boles@hotmail.com>
2023-10-01 18:41:25 -0700

    git commit -m "deleted routes"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './styles/App.css';
-import './styles/Navbar.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './styles/Navbar.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Spotify from "./pages/Spotify";
-import LessonBook from "./pages/LessonBook"
 
 function App() {
   return (
@@ -15,12 +12,6 @@ function App() {
     <header>
       <Navbar />
     </header>
-    <Routes>
-{      /*<Route path='/' element={<Home />}></Route>
-*/}
-      <Route path='/Spotify' element={<Spotify />}/>
-      <Route path='/LessonBook' element={<LessonBook/>}/>
-    </Routes>
 
       <Home />
       <About />
