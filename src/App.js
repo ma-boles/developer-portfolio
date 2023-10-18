3259b42530de359d190ecd3bdcb716dd81c22a2a
ma-boles <ma_boles@hotmail.com>
2023-10-18 15:15:24 -0700

    git commit -m "deleted old import code"

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import React, { lazy, Suspense } from 'react';
 import './styles/App.css';
 import './styles/Navbar.css';
 
-/*import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from "./components/Footer"*/
-
 const Navbar = lazy(() => import('./components/Navbar')); 
 const Home = lazy(() => import('./components/Home')); 
 const About = lazy(() => import('./components/About')); 
